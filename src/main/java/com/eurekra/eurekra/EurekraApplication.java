8f3bc4c0cf2f1e48895e8d713b4eab52c1e453de
geetanjali <geetanjali@cdac.in>
2025-06-11 10:51:24 +0530

    eurekea

---
+++ src/main/java/com/eurekra/eurekra/EurekraApplication.java
@@ -0,0 +1,15 @@
+package com.eurekra.eurekra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eurekra/eurekra/EurekraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eurekra.eurekra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
